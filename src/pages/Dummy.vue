b12ed217e6f7a842879a2439e83c807927851a00
Alen Alic <aalic89@gmail.com>
2019-11-29 23:32:55 +0100

    page view to dummy page

--- src/pages/Dummy.vue
+++ src/pages/Dummy.vue
@@ -2,6 +2,7 @@
   <v-row align="center" justify="center">
     <div class="text-center">
       <h2>Dummy page</h2>
+      <p>{{ $route.name }}</p>
       <img alt="Flask logo" src="../assets/logo-flask.png" height="200" />
       <img alt="Vue logo" src="../assets/logo-vue.png" height="200" />
     </div>
